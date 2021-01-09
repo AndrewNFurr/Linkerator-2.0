c8aa7aa43f50971e48002eb83adc9a68bd7e6f7b
Andrew <oleandy1221@gmail.com>
2021-01-09 13:26:15 -0600

    heroku

--- data_layer/client.js
+++ data_layer/client.js
@@ -3,11 +3,11 @@ const { Client } = require("pg");
 const DATABASE_URL =
   process.env.DATABASE_URL || "postgres://localhost:5432/linkerator";
 
-//const client = new Client(DATABASE_URL);
-const client = new Client({
-  connectionString: DATABASE_URL,
-  ssl: process.env.DATABASE_URL ? {rejectUnauthorized: false} : undefined
-});
+const client = new Client(DATABASE_URL);
+// const client = new Client({
+//   connectionString: DATABASE_URL,
+//   ssl: process.env.DATABASE_URL ? {rejectUnauthorized: false} : undefined
+// });
 
 client.connect();
 
